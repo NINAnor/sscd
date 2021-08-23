380e600c5011279ced202697dfdafa1000cd4fc9
Bruno <bruno@dmpstats.com>
2021-08-23 10:05:28 +0100

    Update helpers.py

--- sscd_libs/helpers.py
+++ sscd_libs/helpers.py
@@ -11,6 +11,8 @@ Module for miscellaneous utility functions
 import shutil
 import os
 from distutils.util import strtobool
+import requests 
+from tqdm import tqdm
 
 # ------------------------------------------------------------------------------
 def boolean_string(s):
@@ -70,4 +72,47 @@ def query_yes_no(question, default='no'):
                 return strtobool(resp)
         except ValueError:
             print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+            
+            
+
+
+
+# ------------------------------------------------------------------------------
+def download_url(url, save_filepath, chunk_size=1024):
+    """
+    Download file from a url path
+
+    Parameters
+    ----------
+    url : str
+        the url address to file of interest
+    save_path : str
+        local path to downloaded file
+    chunk_size : int, optional
+        DESCRIPTION. size of chunk to download in each iteration The default is 128.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    # Streaming, so we can iterate over the response
+    r = requests.get(url, stream=True)
+    
+    #breakpoint()
+    
+    file_size_bytes = int(r.headers.get("content-length", 0))
+
+    #niter = int(file_size/chunk_size)
+    pbar = tqdm(total=file_size_bytes, position=0, leave=True, ascii=True, 
+                desc = "Downloading file", unit = "iB", unit_scale = True) #, ncols=120,
+    
+    with open(save_filepath, 'wb') as file:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            pbar.update(len(chunk))
+            file.write(chunk)
+    
+    pbar.close()
+                
     
